e931629b8064831ddbda191e80f194ab0e53dab0
dpappas <dionysis_pappas@hotmail.com>
2024-05-28 11:09:41 +0300

    My very third commit

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-curl -o data.json https://platform.zone01.gr/assets/superhero/all.json
+curl -o to-git-or-not-to-git.sh  https://platform.zone01.gr/assets/superhero/all.json
 jq -r '.[] | select(.id == 170) | .name, .powerstats.power, .appearance.gender' data.json
