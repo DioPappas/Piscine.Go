cf9161745591f3f2af9c7ce38ef94b0c9333b3ea
dpappas <dionysis_pappas@hotmail.com>
2024-05-30 20:08:50 +0300

    My very sixth go program5

--- printcomb2.go
+++ printcomb2.go
@@ -5,7 +5,9 @@ import (
 )
 
 func PrintComb2() {
+
 	for i := '0'; i <= '9'; i++ {
+
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				for l := '0'; l <= '9'; l++ {
@@ -13,16 +15,17 @@ func PrintComb2() {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
 						z01.PrintRune(' ')
+
 						z01.PrintRune(k)
 						z01.PrintRune(l)
 						if !(i == '9' && j == '8' && k == '9' && l == '9') {
-								z01.PrintRune(',')
-								z01.PrintRune(' ')
-							}
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
 						}
 					}
 				}
 			}
 		}
-		z01.PrintRune('\n')
 	}
+	z01.PrintRune('\n')
+}
