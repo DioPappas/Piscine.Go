8565daf8acc9de4fe6f689ebad59d59ce68b021e
dpappas <dionysis_pappas@hotmail.com>
2024-05-30 19:49:46 +0300

    My very sixth go program1

--- printcomb2.go
+++ printcomb2.go
@@ -6,10 +6,10 @@ import (
 
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
-		for j :='0'; j <= '9'; j++ {
-			for k :='0'; k<= '9'; k++ {
-				for l :='0'; l<='9'; l++ {
-					if (i<k) || (i==k && j<l) {
+		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				for l := '0'; l <='9'; l++ {
+					if (i < k) || (i == k && j < l) {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
 						z01.PrintRune(' ')
