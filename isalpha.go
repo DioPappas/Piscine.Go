d2084883c4da9722f9dcf78362efe339a0faa78f
dpappas <dionysis_pappas@hotmail.com>
2024-06-04 20:49:39 +0300

    quest05_10

--- isalpha.go
+++ isalpha.go
@@ -1,10 +1,8 @@
 package piscine
 
-import "unicode"
-
 func IsAlpha(s string) bool {
 	for _, char := range s {
-		if !unicode.IsLetter(char) && !unicode.IsDigit(char) {
+		if !(char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z' || char >= '0' && char <= '9') {
 			return false
 		}
 	}
