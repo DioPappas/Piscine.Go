284fff563cd52219cd3a6792063c9f3d12b92f28
dpappas <dionysis_pappas@hotmail.com>
2024-06-04 20:05:47 +0300

    quest05_05

---
+++ alphacount.go
@@ -0,0 +1,12 @@
+package piscine
+
+func AlphaCount(s string) int {
+	runes := []rune(s)
+	count := 0
+	for i := range runes {
+		if runes[i] >= 'A' && runes[i] <= 'Z' || runes[i] >= 'a' && runes[i] <= 'z' {
+			count++
+		}
+	}
+	return count
+}
