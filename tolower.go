5a8188fc862006c0ed8e77976f58d08fb5c16f78
dpappas <dionysis_pappas@hotmail.com>
2024-06-05 17:46:48 +0300

    quest05_13

--- tolower.go
+++ tolower.go
@@ -4,7 +4,7 @@ func ToLower(s string) string {
 	result := ""
 	for _, char := range s {
 		if char >= 'A' && char <= 'Z' {
-			result += string(char - 32)
+			result += string(char + 32)
 		} else {
 			result += string(char)
 		}
