7b03a0f5c6fd4859b302040d0a8feabe027ec630
dpappas <dionysis_pappas@hotmail.com>
2024-06-06 16:59:30 +0300

    quest06_04

--- sortparams/main.go
+++ sortparams/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"os"
-	"sort"
 
 	"github.com/01-edu/z01"
 )
@@ -11,15 +10,22 @@ func main() {
 	// os.Args contains the command-line arguments passed to the program
 	args := os.Args[1:] // Skip the first argument which is the program name
 
-	// Sort the arguments in alphabetical order
-	sort.Strings(args)
+	// Implement bubble sort to sort the arguments
+	n := len(args)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if args[j] > args[j+1] {
+				args[j], args[j+1] = args[j+1], args[j]
+			}
+		}
+	}
 
 	// Loop through each sorted argument
 	for _, arg := range args {
 		// Loop through each character in the argument
 		for _, r := range arg {
-			z01.PrintRune(r)
+			z01.PrintRune(r) // Print each character
 		}
-		z01.PrintRune('\n')
+		z01.PrintRune('\n') // Print a newline after each argument
 	}
 }
