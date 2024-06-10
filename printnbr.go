61e046f65e478368fd4911c041b790c60af8f671
dpappas <dionysis_pappas@hotmail.com>
2024-06-10 18:46:11 +0300

    quest02_07

--- printnbr.go
+++ printnbr.go
@@ -5,19 +5,25 @@ import (
 )
 
 // PrintNbr prints an int passed as a parameter
-func PrintNbr(n int) {
+func PrintNbr(n int64) {
 	if n == 0 {
 		z01.PrintRune('0')
 		return
 	}
 	if n < 0 {
-		z01.PrintRune('-')
-		n = -n
+		if n == -9223372036854775808 { // Special case for the minimum int64 value
+			z01.PrintRune('-')
+			z01.PrintRune('9')
+			n = 223372036854775808 // Set n to the positive overflow value of int64
+		} else {
+			z01.PrintRune('-')
+			n = -n
+		}
 	}
 	printPositive(n)
 }
 
-func printPositive(n int) {
+func printPositive(n int64) {
 	if n == 0 {
 		return
 	}
