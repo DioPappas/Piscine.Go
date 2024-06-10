47302a0c1859f5237a4b2f2bd9bb2d729605ef0f
dpappas <dionysis_pappas@hotmail.com>
2024-06-10 17:58:55 +0300

    quest08_04

--- cat/main.go
+++ cat/main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
 	args := os.Args[1:]
 
 	if len(args) == 0 {
-		// If no arguments are provided, read from stdin
-		cat(os.Stdin)
+		stat, _ := os.Stdin.Stat()
+		if (stat.Mode() & os.ModeCharDevice) == 0 {
+			cat(os.Stdin)
+		} else {
+			printError("No input provided through pipe\n")
+			os.Exit(1)
+		}
 		return
 	}
 
 	for _, arg := range args {
 		file, err := os.Open(arg)
 		if err != nil {
-			fmt.Printf("ERROR: open %s: %v\n", arg, err)
+			printError(err.Error() + "\n")
 			continue
 		}
 		cat(file)
@@ -29,8 +35,16 @@ func main() {
 func cat(reader io.Reader) {
 	content, err := io.ReadAll(reader)
 	if err != nil {
-		fmt.Printf("ERROR: %v\n", err)
+		printError(err.Error() + "\n")
 		return
 	}
-	fmt.Print(string(content))
+	for _, ch := range content {
+		z01.PrintRune(rune(ch))
+	}
+}
+
+func printError(msg string) {
+	for _, ch := range msg {
+		z01.PrintRune(ch)
+	}
 }
