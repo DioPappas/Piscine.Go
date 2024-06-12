b7fb8c96c9d6b5251507470d8087150cf0107419
dpappas <dionysis_pappas@hotmail.com>
2024-06-12 14:35:18 +0300

    quest09_01

--- foreach.go
+++ foreach.go
@@ -1,15 +1,19 @@
+// piscine/foreach.go
 package piscine
 
-import "fmt"
-
-// PrintNumber prints a single integer
-func PrintNumber(n int) {
-	fmt.Print(n)
-}
+import (
+	"os"
+	"strconv"
+)
 
 // ForEach applies a given function to each element of an integer slice
 func ForEach(f func(int), a []int) {
 	for _, v := range a {
 		f(v)
 	}
+}
+
+// Directly prints a single integer without using fmt
+func PrintInt(n int) {
+	os.Stdout.WriteString(strconv.Itoa(n))
 }
