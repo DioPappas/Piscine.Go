bd97db10c8b838cb004d5936a5135806e77b19fb
dpappas <dionysis_pappas@hotmail.com>
2024-06-12 14:38:56 +0300

    quest09_01

--- foreach.go
+++ foreach.go
@@ -1,32 +1,7 @@
-// piscine/foreach.go
 package piscine
 
-import "os"
-
-// PrintInt prints a single integer without using fmt or additional packages
-func PrintInt(n int) {
-	if n == 0 {
-		os.Stdout.WriteString("0")
-		return
-	}
-
-	if n < 0 {
-		os.Stdout.WriteString("-")
-		n = -n
-	}
-
-	var digits []byte
-	for n > 0 {
-		digits = append([]byte{byte(n%10 + '0')}, digits...)
-		n /= 10
-	}
-
-	os.Stdout.Write(digits)
-}
-
-// ForEach applies a given function to each element of an integer slice
-func ForEach(f func(int), a []int) {
-	for _, v := range a {
-		f(v)
+func ForEach(f func(int), arr []int) {
+	for _, res := range arr {
+		f(res)
 	}
 }
