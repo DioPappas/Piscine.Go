cdc39634aa41435b32d9d410ff67125eab6c0ad1
dpappas <dionysis_pappas@hotmail.com>
2024-06-12 14:40:10 +0300

    quest09_02

---
+++ map.go
@@ -0,0 +1,10 @@
+package piscine
+
+// Map applies a function of type func(int) bool on each element of an int slice and returns a slice of all the return values
+func Map(f func(int) bool, a []int) []bool {
+	result := make([]bool, len(a))
+	for i, v := range a {
+		result[i] = f(v)
+	}
+	return result
+}
