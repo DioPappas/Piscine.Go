a157ed002c2cbac0abd0b7879b4f53e6963c5f5c
dpappas <dionysis_pappas@hotmail.com>
2024-06-13 13:29:19 +0300

    hackathon_03

--- unmatch.go
+++ unmatch.go
@@ -1,17 +1,21 @@
 package piscine
 
 func Unmatch(a []int) int {
+	// Create a map to count occurrences of each element
 	count := make(map[int]int)
 
+	// Count occurrences of each element in the slice
 	for _, num := range a {
 		count[num]++
 	}
 
+	// Find the element with an odd count (unmatched element)
 	for num, cnt := range count {
 		if cnt%2 != 0 {
 			return num
 		}
 	}
 
+	// If all elements have pairs, return -1
 	return -1
 }
