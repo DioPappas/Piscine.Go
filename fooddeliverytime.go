a41b0ed23d6e4e2fdad6cd376207f12d65c6c0b5
dpappas <dionysis_pappas@hotmail.com>
2024-06-13 13:41:12 +0300

    hackathon_04

--- fooddeliverytime.go
+++ fooddeliverytime.go
@@ -1,22 +1,19 @@
 package piscine
 
-// Define a struct for food items
 type food struct {
 	preptime int
 }
 
-// Map to store menu items and their preparation times
 var menu = map[string]food{
 	"burger":  {preptime: 15},
 	"chips":   {preptime: 10},
 	"nuggets": {preptime: 12},
 }
 
-// FoodDeliveryTime function to get preparation time for an item
 func FoodDeliveryTime(order string) int {
 	if item, ok := menu[order]; ok {
 		return item.preptime
 	}
-	// Return 404 if item not found in menu
+
 	return 404
 }
