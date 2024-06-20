d0436a04288aa266b7b21eda7cedfce8f72d66d7
dpappas <dionysis_pappas@hotmail.com>
2024-06-20 18:12:50 +0300

    descendappendrange

--- descendappendrange.go
+++ descendappendrange.go
@@ -1,18 +1,13 @@
 package piscine
 
 func DescendAppendRange(max, min int) []int {
-	// Check if max is less than or equal to min
 	if max <= min {
-		return []int{} // Return empty slice
+		return []int{}
 	}
 
-	// Calculate the number of elements in the range
 	size := max - min
-
-	// Create a slice with size elements
 	result := make([]int, size)
 
-	// Populate the slice in descending order
 	for i := 0; i < size; i++ {
 		result[i] = max - i - 1
 	}
