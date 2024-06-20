eebc05e94b1fa1129f56506885f5f8e162040c54
dpappas <dionysis_pappas@hotmail.com>
2024-06-20 18:09:52 +0300

    enigma

---
+++ enigma.go
@@ -0,0 +1,15 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	// Temporary variables to hold intermediate values
+	tempA := ***a
+	tempB := *b
+	tempC := *******c
+	tempD := ****d
+
+	// Assignments as per the specified rules
+	*******c = tempA
+	****d = tempC
+	*b = tempD
+	***a = tempB
+}
